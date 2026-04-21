a349423079720599d1485e6b64c4451afa3a2af3
Nitish Bhardwaj <nitish@obrive.com>
2026-04-21 18:42:59 +0530

    fix

--- src/components/layout/Container.tsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children }) => {
-  return (
-    <div className="w-full max-w-[1450px] mx-auto px-6 sm:px-10 lg:px-12">
-      {children}
-    </div>
-  );
-};
-
-export default Container;
