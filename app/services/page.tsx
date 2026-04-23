542418ee4faa03fc1eda3b1b305c2437be4ce573
Nitish Bhardwaj <nitish@obrive.com>
2026-04-23 18:24:09 +0530

    rename

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
-import BrandsSection from "@/components/HomePage/BrandsSection"
-import ContactCTA from "@/components/HomePage/ContactCTA"
+import BrandsSection from "@/components/homepage/BrandsSection"
+import ContactCTA from "@/components/homepage/ContactCTA"
 import Footer from "@/components/layout/Footer"
 import ConnectedThinkingServices from "@/components/ServicesPage/ConnectedThinking"
 import PerformSection from "@/components/ServicesPage/PerformSection"
